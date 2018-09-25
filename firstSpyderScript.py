8405df66081a6df3c095fb34b45ee2d84864330e
Remko Weijers <30292426+weij3721@users.noreply.github.com>
2018-09-25 21:36:19 +0200

    fixed the import pandas command
    forgot to add the abbreviation

--- firstSpyderScript.py
+++ firstSpyderScript.py
@@ -9,8 +9,8 @@ textToPrint = "Remko"
 print (textToPrint)
 
 #try to add some machine learning to this
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 
 #first read data
 data = pd.read_csv('D:/Kaggle/FifaDataset/FIFA 2018 Statistics.csv')
