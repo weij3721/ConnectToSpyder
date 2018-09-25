8f3545f9c2cc4ce15f70aee7933ee8a65d4120c0
Remko Weijers <30292426+weij3721@users.noreply.github.com>
2018-09-25 21:34:22 +0200

    import data

--- firstSpyderScript.py
+++ firstSpyderScript.py
@@ -7,3 +7,10 @@ Created on Tue Sep 25 21:27:17 2018
 
 textToPrint = "Remko"
 print (textToPrint)
+
+#try to add some machine learning to this
+import numpy
+import pandas
+
+#first read data
+data = pd.read_csv('D:/Kaggle/FifaDataset/FIFA 2018 Statistics.csv')
