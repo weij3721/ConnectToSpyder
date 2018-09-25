7e344d6d6c474c6dc092b488359b10eb8c18c8f8
Remko Weijers <30292426+weij3721@users.noreply.github.com>
2018-09-25 23:40:38 +0200

    added project and ran test based on function in another file

--- myfirstunit_test.py
+++ myfirstunit_test.py
@@ -7,10 +7,12 @@ Created on Tue Sep 25 22:54:29 2018
 
 #my first python unit test
 
-def calculator_add(x,y):
-    z = x + y
-    return z
+#def calculator_add(x,y):
+#   z = x + y
+#   return z
+
+from firstSpyderScript import calculator_add
     
 def test_answer():
-    assert calculator_add(1,2) == 4
+    assert calculator_add(1,3) == 4
     
